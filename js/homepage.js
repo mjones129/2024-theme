c9af8ea656ada764eb25dcf3c929058f199cdefb
Matt Jones <me@mattjones.tech>
2024-08-19 14:28:35 -0400

    add film grain to canvas

--- js/homepage.js
+++ js/homepage.js
@@ -1,71 +1,88 @@
 import * as THREE from "three";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { FilmPass } from "three/addons/postprocessing/FilmPass.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 
 function main() {
+  const canvas = document.getElementById("hero");
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.setPixelRatio(window.devicePixelRatio); // Handle high-DPI displays
 
-	const canvas = document.getElementById('hero');
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+  const fov = 75;
+  const aspect = canvas.clientWidth / canvas.clientHeight;
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-	const fov = 75;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+  const scene = new THREE.Scene();
 
-	const scene = new THREE.Scene();
+  {
+    const color = 0xffffff;
+    const intensity = 3;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
-	{
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-		const color = 0xFFFFFF;
-		const intensity = 3;
-		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( - 1, 2, 4 );
-		scene.add( light );
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
 
-	}
+  const cube = new THREE.Mesh(geometry, material);
 
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+  scene.add(cube);
 
-	const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } ); // greenish blue
+  //postproduction
+  const composer = new EffectComposer(renderer);
+  composer.addPass(new RenderPass(scene, camera));
+  const filmPass = new FilmPass(
+    0.5, //intensity
+    false, //greyscale
+  );
+  composer.addPass(filmPass);
+  const outputPass = new OutputPass();
+  composer.addPass(outputPass);
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth * window.devicePixelRatio;
+    const height = canvas.clientHeight * window.devicePixelRatio;
+    const needResize = canvas.width !== width || canvas.height !== height;
 
-	function render( time ) {
-
-		time *= 0.001; // convert time to seconds
-
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+      composer.setSize(width, height);
     }
+    return needResize;
+  }
+
+  let then = 0;
+
+  function render(now) {
+    now *= 0.001; // convert time to seconds
+    const deltaTime = now - then;
+    then = now;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
-    } 
+    }
 
-		cube.rotation.x = time;
-		cube.rotation.y = time;
+    cube.rotation.x = now;
+    cube.rotation.y = now;
 
-		renderer.render( scene, camera );
+    composer.render(deltaTime);
 
-		requestAnimationFrame( render );
+    requestAnimationFrame(render);
+  }
 
-	}
-
-	requestAnimationFrame( render );
-
+  requestAnimationFrame(render);
 }
 
 main();
--- patterns/front-page.php
+++ patterns/front-page.php
@@ -8,8 +8,10 @@
 ?>
 <!-- wp:group {"metadata":{"categories":["header"],"patternName":"core/header-inside-full-width-background-image","name":"Header inside full-width background image"},"align":"full","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull">
-<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:template-part {"slug":"header","area":"header"} /-->
-
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:50px">
+<span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim"></span>
+<div class="wp-block-cover__inner-container">
+<!-- wp:template-part {"slug":"header","area":"header"} /-->
 <canvas id="hero"></canvas>
 
 <!-- wp:heading {"customTextColor":"#e9e9e9" "textAlign":"center","level":1} -->
