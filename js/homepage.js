aca838dc716e184c731fb3a22b8a0705b6efd010
Matt Jones <me@mattjones.tech>
2024-08-19 08:07:11 -0400

    fix resolution

--- js/homepage.js
+++ js/homepage.js
@@ -38,6 +38,23 @@ function main() {
 
 		time *= 0.001; // convert time to seconds
 
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    } 
+
 		cube.rotation.x = time;
 		cube.rotation.y = time;
 
