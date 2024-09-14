5e0b13b6df711981fee73103359c485d11e8f8d7
Matt Jones <me@mattjones.tech>
2024-09-14 15:36:17 -0400

    disable webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./js/homepage.js",
-  watch: true,
+  // watch: true,
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
