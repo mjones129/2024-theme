e2484dd73589c828eef4c367ebc405f85f5207a9
Matt Jones <me@mattjones.tech>
2024-09-14 13:57:13 -0400

    import logo model

--- js/homepage.js
+++ js/homepage.js
@@ -1,88 +1,193 @@
 import * as THREE from "three";
-import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
-import { FilmPass } from "three/addons/postprocessing/FilmPass.js";
-import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+import GUI from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-function main() {
-  const canvas = document.getElementById("hero");
-  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-  renderer.setPixelRatio(window.devicePixelRatio); // Handle high-DPI displays
+/**
+ * Debug
+ */
+const gui = new GUI();
 
-  const fov = 75;
-  const aspect = canvas.clientWidth / canvas.clientHeight;
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+const parameters = {
+  materialColor: "#898dad",
+};
 
-  const scene = new THREE.Scene();
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
+});
 
-  {
-    const color = 0xffffff;
-    const intensity = 3;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-  }
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+// Scene
+const scene = new THREE.Scene();
 
-  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
+/**
+ * Objects
+ */
 
-  const cube = new THREE.Mesh(geometry, material);
+//texture
+const textureLoader = new THREE.TextureLoader();
 
-  scene.add(cube);
+//material
+const material = new THREE.MeshStandardMaterial({
+  color: parameters.materialColor,
+});
 
-  //postproduction
-  const composer = new EffectComposer(renderer);
-  composer.addPass(new RenderPass(scene, camera));
-  const filmPass = new FilmPass(
-    0.5, //intensity
-    false, //greyscale
-  );
-  composer.addPass(filmPass);
-  const outputPass = new OutputPass();
-  composer.addPass(outputPass);
+//models
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+  "/wp-content/themes/2024-theme/assets/models/mjt-logo.glb",
+  (gltf) => {
+    scene.add(gltf.scene);
+  },
+);
 
-  function resizeRendererToDisplaySize(renderer) {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth * window.devicePixelRatio;
-    const height = canvas.clientHeight * window.devicePixelRatio;
-    const needResize = canvas.width !== width || canvas.height !== height;
+/**
+ * Particles
+ */
+//Geometry
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
 
-    if (needResize) {
-      renderer.setSize(width, height, false);
-      composer.setSize(width, height);
-    }
-    return needResize;
-  }
-
-  let then = 0;
-
-  function render(now) {
-    now *= 0.001; // convert time to seconds
-    const deltaTime = now - then;
-    then = now;
-
-    if (resizeRendererToDisplaySize(renderer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
-
-    cube.rotation.x = now;
-    cube.rotation.y = now;
-
-    composer.render(deltaTime);
-
-    requestAnimationFrame(render);
-  }
-
-  requestAnimationFrame(render);
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
 }
 
-main();
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3),
+);
+
+//material
+
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
+
+//points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight("#fff", 3);
+directionalLight.position.set(1, 1, 0);
+directionalLight.lookAt(0, 0, 0);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+//group
+
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.z = 6;
+cameraGroup.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: false,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Scroll
+ */
+
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+  const newSection = Math.round(scrollY / sizes.height);
+});
+
+/**
+ * Cursor
+ */
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  //animate camera
+
+  camera.position.y = -scrollY / sizes.height;
+  const parallaxX = cursor.x;
+  const parallaxY = -cursor.y;
+
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 1.2 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 1.2 * deltaTime;
+
+  //animate meshes
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./js/homepage.js",
+  watch: true,
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
